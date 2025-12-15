39e8fad885aef42c34cfc98bb5649672a5a864d8
Dhruv Sonavane <sonavanebharat92@gmail.com>
2025-12-15 14:14:15 +0530

    .

--- index.cpp
+++ index.cpp
@@ -9,7 +9,16 @@ double ascii(string, string);
 
 int main()
 {
-    cout << ascii("c" ,"vocab.json");
+    string text;
+    cout << "Enter :- " ;
+    getline(cin, text);
+    for (int i = 0; i < text.size(); ++i)
+    {
+        string key(1, text[i]);
+        double value = ascii(key ,"vocab.json");
+        if (value==1) break;
+        else cout << key << " = " << value << endl;
+    }
     return 0;
 }
 
@@ -22,21 +31,22 @@ double ascii(string key, string path)
         cerr << "Failed to open vocab.json\n";
         return 1;
     }
-    json data;
-    try
+    else
     {
-        file >> data;
+        json data;
+        try
+        {
+            file >> data;
+        }
+        catch (const json::parse_error& e)
+        {
+            cerr << "JSON parse error: " << e.what() << "\n";
+            return 1;
+        }
+        if (data.contains(key)) value = data.at(key).get<double>();
+        else cout << key << " Not found in this file " << endl;
+        file.close();
+
     }
-    catch (const json::parse_error& e)
-    {
-        cerr << "JSON parse error: " << e.what() << "\n";
-        return 1;
-    }
-    if (data.contains(key))
-    {
-        value = data.at(key).get<double>();
-    }
-    else cout << key << " Not found in this file " << endl;
-    file.close();
     return value;
 }
