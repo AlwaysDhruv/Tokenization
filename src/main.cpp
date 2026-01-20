e417b4aa5a15636aac709558b1aa99864e8c1de9
Dhruv Sonavane <sonavanebharat92@gmail.com>
2026-01-20 14:21:53 +0530

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "./include/BPE.hpp" //Encoding And Decoding with
+#include "./include/BPE.hpp"
 using namespace std;
 
 int main()
@@ -9,13 +9,13 @@ int main()
     vector<long long> token;
     vector<string> pair;
     
-    tk.fit("../data/test.txt", 100); //Training
-    tk.encoding("../data/test2.txt", pair, token); // Testing with much of tokens
+    tk.fit("../data/test.txt", 100);
+    tk.encoding("../data/test2.txt", pair, token);
 
     for (int i = 0; i < pair.size(); ++i) cout << token[i] << " ";
     cout << endl << endl;
     
-    cout << tk.decoding(355) << endl; //Decoding
+    cout << tk.decoding(355) << endl;
     
     return 0;
 }
